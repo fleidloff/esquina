2d016a99e1f7749e76838c0be07124eeca9fc556
Frederik Leidloff <kontakt@frederik-leidloff.de>
2016-09-23 09:42:11 +0200

    fixed header sticky without js

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-var header = document.getElementsByClassName("header")[0];
-document.addEventListener("scroll", function(ev) {
-  	if (document.body.scrollTop > window.innerHeight) {
-    	document.body.className = "fix-header";  
-	} else {
-   		document.body.className = "";  
-  	}
-});
